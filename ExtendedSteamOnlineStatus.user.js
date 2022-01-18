b2fcafa02d612283526a9595ebcb8e6f360f0caa
NetroScript <noreply@enostrion.com>
2022-01-18 02:18:50 +0100

    Add comments and improve structure of the code

--- ExtendedSteamOnlineStatus.user.js
+++ ExtendedSteamOnlineStatus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ExtendedSteamOnlineStatus
-// @namespace    https://github.com/NetroScript
+// @namespace    https://github.com/NetroScript/ExtendedSteamOnlineStatus
 // @version      0.1
 // @description  Add Busy, Looking to Play and Looking to Trade back to the Steam Chat
 // @author       NetroScript
@@ -11,74 +11,117 @@
 // @require      https://code.jquery.com/jquery-3.6.0.slim.js
 // ==/UserScript==
 
-
+// Helper function to set the current online state
 window.setPersonaState = (state = 1) => {
-    let buffer = new Uint8Array(("cc0200800f00000009ecbcdd0701001001108ccba08c02080"+state+"280138004000").match(/[\da-f]{2}/gi).map(function (h) {
-        return parseInt(h, 16)}));
-    window.WebSocket.instances.filter(socket => socket.readyState == 1)[0].send(buffer.buffer)
+    // Create an Array Buffer fitting for the ProtoBuf object which is used to send the persona state message
+    /*
+    Following Online states are possible:
+    enum EPersonaState {
+        'Offline' = 0,
+        'Online' = 1,
+        'Busy' = 2,
+        'Away' = 3,
+        'Snooze' = 4,
+        'LookingToTrade' = 5,
+        'LookingToPlay' = 6,
+        'Invisible' = 7,
+        'Max' = 8,
+    } */
+    let buffer = new Uint8Array([204, 2, 0, 128, 15, 0, 0, 0, 9, 236, 188, 221, 7, 1, 0, 16, 1, 16, 140, 203, 160, 140, 2, 8, state, 40, 1, 56, 0, 64, 0]);
+
+    // If we don't have a global steam websocket yet, declare it
+    if (!window.hasOwnProperty("SteamWebSocket")) {
+        // Pick the first connected websocket;
+        window.SteamWebSocket = window.WebSocket.instances.filter(socket => socket.readyState === 1)[0];
+    }
+
+    // Send our specific buffer;
+    window.SteamWebSocket.send(buffer.buffer)
 }
 
 (function() {
     'use strict';
 
+    // We don't make use of it, but just keep a copy of it, just to be sure
     const original_websocket = WebSocket;
 
+    // Extend the WebSocket class of the browser, to be able to hook into its creation
     class CustomWebsocket extends WebSocket {
 
+        // Statically keep a list of all ever created websocket instances
         static instances = [];
 
+        // On creation, log our websocket
         constructor(){
             console.log("Creating Websocket: ", ...arguments)
+            // Call the default construction
             super(...arguments);
+            // Add the new instance to our list of instances
             CustomWebsocket.instances.push(this);
         }
     }
 
+    // Overwrite the old object with the newly created class.
     window.WebSocket = CustomWebsocket
 
-
+    // When the document is ready
     $(() => {
 
+        // Create an observer
         const observer = new MutationObserver((mutations)=>{
+            // Keep track of added nodes
             let addedNodes = [];
 
+            // Iterate every mutation
             mutations.forEach(mutation => {
-                console.log(mutation)
+                // For each mutation, iterate the newly added nodes
                 mutation.addedNodes.forEach(node => {
 
+                    // Store the added node
                     addedNodes.push(node);
+
+                    // Create a variable referring to the current node
                     const el = $(node);
 
+                    // Check if the newly created DOM element is the user persona status drop down
                     if(el.find(".persona_menu_dnd_descriptor").length > 0) {
 
-                        el.find(".contextMenuSectionContent").children().first().clone().text("Busy").addClass("busy-status-button").removeClass("menuChecked").insertAfter(el.find(".contextMenuSectionContent").children().last())
-                        el.find(".contextMenuSectionContent").children().first().clone().text("Looking to trade").addClass("trade-status-button").removeClass("menuChecked").insertAfter(el.find(".contextMenuSectionContent").children().last())
-                        el.find(".contextMenuSectionContent").children().first().clone().text("Looking to play").addClass("play-status-button").removeClass("menuChecked").insertAfter(el.find(".contextMenuSectionContent").children().last())
+                        // create a utility function to add an online status
 
-                        $(".busy-status-button").click(()=>{
-                            window.setPersonaState(2);
-                            addedNodes.forEach(entry => $(entry).hide());
-                        });
+                        const addAdditionalButton = (displayName, personaState = 1) => {
+                            // Create a clone of the first button to have the correct css names
+                            el.find(".contextMenuSectionContent").children().first().clone()
+                                // Remove the default selected class if it comes from online
+                                .removeClass("menuChecked")
+                                // Now add the active class, if the current display text of the status is the same as the wanted online status
+                                .addClass(($(".AvatarAndUser > div > div > div").last().text() === displayName) ? "menuChecked" : "")
+                                // Set the displayed string
+                                .text(displayName)
+                                // Add it at the end of the dropdown (of the normal states)
+                                .insertAfter(el.find(".contextMenuSectionContent").children().last())
+                                // Bind the on click event
+                                .click(()=>{
+                                    // Use the utility function to set the state
+                                    window.setPersonaState(personaState);
+                                    // hide all newly added nodes so the user doesn't see the menu anymore
+                                    // and the steam react UI doesn't lose reference to its own menu
+                                    addedNodes.forEach(entry => $(entry).hide());
+                                });
 
-                        $(".trade-status-button").click(()=>{
-                            window.setPersonaState(5);
-                            addedNodes.forEach(entry => $(entry).hide());
-                        });
+                        }
 
-                        $(".play-status-button").click(()=>{
-                            window.setPersonaState(6);
-                            addedNodes.forEach(entry => $(entry).hide());
-                        });
+                        // Add all our new online persona status buttons which were removed in Steam previously
+                        addAdditionalButton("Busy", 2)
+                        addAdditionalButton("Looking to Trade", 5)
+                        addAdditionalButton("Looking to Play", 6)
                     }
 
                 })
             })
         })
 
+        // Watch all changes of the body element (just concering changing children)
         observer.observe(document.body, {childList: true})
-
     })
-
-
 })();
 
